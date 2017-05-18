0abe34a72cf1763cb7aaaff7e0829c7eb902b082
Maximovski <wasdtera@gmail.com>
2017-05-18 18:50:11 +0400

    Initial Commit

---
+++ ValuteConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ValuteConverter
+//
+//  Created by Вячеслав Максимовский on 18.05.17.
+//  Copyright © 2017 Вячеслав Максимовский. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ValuteConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ValuteConverter
+//
+//  Created by Вячеслав Максимовский on 18.05.17.
+//  Copyright © 2017 Вячеслав Максимовский. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
